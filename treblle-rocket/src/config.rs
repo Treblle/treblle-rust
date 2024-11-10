3e5feb6170220d486c96151de2fc0f6f3a4abfa1
momo-gg <momir.zmijanac@happening.xyz>
2024-11-10 22:52:24 +0100

    chore(rocket): move tests into separate file for now

--- treblle-rocket/src/config.rs
+++ treblle-rocket/src/config.rs
@@ -33,20 +33,3 @@ impl Default for RocketConfig {
         }
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_rocket_config() {
-        let mut config = RocketConfig::new("test_key".to_string(), "test_project".to_string());
-        config.add_masked_fields(vec!["password".to_string()]);
-        config.add_ignored_routes(vec!["/health".to_string()]);
-
-        assert_eq!(config.core.api_key, "test_key");
-        assert_eq!(config.core.project_id, "test_project");
-        assert!(config.core.masked_fields.iter().any(|r| r.as_str().contains("password")));
-        assert!(config.core.ignored_routes.iter().any(|r| r.as_str().contains("/health")));
-    }
-}
--- treblle-rocket/src/tests.rs
+++ treblle-rocket/src/tests.rs
@@ -8,7 +8,19 @@ pub mod tests {
         routes,
     };
     use serde_json::{json, Value};
-    use crate::{Treblle, TreblleState};
+    use crate::{RocketConfig, Treblle, TreblleState};
+
+    #[test]
+    fn test_rocket_config() {
+        let mut config = RocketConfig::new("test_key".to_string(), "test_project".to_string());
+        config.add_masked_fields(vec!["password".to_string()]);
+        config.add_ignored_routes(vec!["/health".to_string()]);
+
+        assert_eq!(config.core.api_key, "test_key");
+        assert_eq!(config.core.project_id, "test_project");
+        assert!(config.core.masked_fields.iter().any(|r| r.as_str().contains("password")));
+        assert!(config.core.ignored_routes.iter().any(|r| r.as_str().contains("/health")));
+    }
 
     // Test route handlers
     #[post("/echo", format = "json", data = "<input>")]
