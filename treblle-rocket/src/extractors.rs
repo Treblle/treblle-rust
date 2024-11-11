b4b7aadc5f82b712f1afafc6de8d564a404dc658
momo-gg <momir.zmijanac@happening.xyz>
2024-11-11 14:42:15 +0100

    feat(rocket): add better server info and IP lookup

--- treblle-rocket/src/extractors.rs
+++ treblle-rocket/src/extractors.rs
@@ -1,11 +1,16 @@
 use chrono::Utc;
 use rocket::{Request, Response};
 use serde_json::Value;
-use std::{sync::RwLock, time::Duration};
+use std::{sync::RwLock, time::Duration, sync::OnceLock};
 use treblle_core::{
     payload::HttpExtractor,
-    schema::{ErrorInfo, RequestInfo, ResponseInfo},
+    schema::{ErrorInfo, RequestInfo, ResponseInfo, ServerInfo, OsInfo},
+    utils::extract_ip_from_headers,
 };
+use http::HeaderMap;
+use rocket::http::Status;
+
+static SERVER_INFO: OnceLock<ServerInfo> = OnceLock::new();
 
 /// State key for storing request/response bodies
 #[derive(Default)]
@@ -16,11 +21,67 @@ pub struct TreblleState {
 
 pub struct RocketExtractor;
 
+impl RocketExtractor {
+    fn get_server_info() -> &'static ServerInfo {
+        SERVER_INFO.get_or_init(|| {
+            let os_info = os_info::get();
+            ServerInfo {
+                ip: local_ip_address::local_ip()
+                    .map(|ip| ip.to_string())
+                    .unwrap_or_else(|_| "unknown".to_string()),
+                timezone: time::UtcOffset::current_local_offset()
+                    .map(|o| o.to_string())
+                    .unwrap_or_else(|_| "UTC".to_string()),
+                software: Some(format!("rocket/{}", env!("CARGO_PKG_VERSION"))),
+                signature: None,
+                protocol: "HTTP/1.1".to_string(),
+                encoding: None,
+                os: OsInfo {
+                    name: std::env::consts::OS.to_string(),
+                    release: os_info.version().to_string(),
+                    architecture: std::env::consts::ARCH.to_string(),
+                },
+            }
+        })
+    }
+
+    fn construct_full_url(req: &Request<'_>) -> String {
+        let scheme = if req.headers().get_one("X-Forwarded-Proto").map(|h| h == "https").unwrap_or(false) {
+            "https"
+        } else {
+            "http"
+        };
+
+        let host = req.headers().get_one("Host").unwrap_or("localhost");
+        format!("{}://{}{}", scheme, host, req.uri())
+    }
+
+    fn convert_headers_to_http(headers: &rocket::http::HeaderMap<'_>) -> HeaderMap {
+        let mut http_headers = HeaderMap::new();
+        for header in headers.iter() {
+            if let (Ok(name), Ok(value)) = (
+                http::header::HeaderName::from_bytes(header.name.as_str().as_bytes()),
+                http::header::HeaderValue::from_str(&header.value.to_string())
+            ) {
+                http_headers.insert(name, value);
+            }
+        }
+        http_headers
+    }
+
+    fn is_error_status(status: Status) -> bool {
+        status.code >= 400
+    }
+}
+
 impl HttpExtractor for RocketExtractor {
     type Request = Request<'static>;
     type Response = Response<'static>;
 
     fn extract_request_info(req: &Self::Request) -> RequestInfo {
+        let headers = Self::convert_headers_to_http(req.headers());
+
+        // Use rocket() to get access to state in Rocket 0.5
         let body = req.rocket()
             .state::<TreblleState>()
             .and_then(|state| state.request_body.read().ok())
@@ -28,10 +89,10 @@ impl HttpExtractor for RocketExtractor {
 
         RequestInfo {
             timestamp: Utc::now(),
-            ip: req.client_ip()
-                .map(|addr| addr.to_string())
+            ip: extract_ip_from_headers(&headers)
+                .or_else(|| req.client_ip().map(|addr| addr.to_string()))
                 .unwrap_or_else(|| "unknown".to_string()),
-            url: req.uri().to_string(),
+            url: Self::construct_full_url(req),
             user_agent: req.headers().get_one("User-Agent")
                 .unwrap_or("")
                 .to_string(),
@@ -44,9 +105,8 @@ impl HttpExtractor for RocketExtractor {
     }
 
     fn extract_response_info(res: &Self::Response, duration: Duration) -> ResponseInfo {
-        // Get content length using Rocket's header access methods
         let size = res.headers()
-            .get_one("content-length")
+            .get_one("Content-Length")
             .and_then(|val| val.parse::<u64>().ok())
             .unwrap_or(0);
 
@@ -62,19 +122,25 @@ impl HttpExtractor for RocketExtractor {
     }
 
     fn extract_error_info(res: &Self::Response) -> Option<Vec<ErrorInfo>> {
-        if res.status().class().is_server_error() || res.status().class().is_client_error() {
+        if Self::is_error_status(res.status()) {
+            // For Rocket responses, we'll use status code and reason
+            let message = format!("HTTP {} {}",
+                                  res.status().code,
+                                  res.status().reason().unwrap_or(""));
+
             Some(vec![ErrorInfo {
                 source: "rocket".to_string(),
                 error_type: format!("HTTP_{}", res.status().code),
-                message: format!("HTTP {} {}",
-                                 res.status().code,
-                                 res.status().reason().unwrap_or("")
-                ),
+                message,
                 file: String::new(),
                 line: 0,
             }])
         } else {
             None
         }
+    }
+
+    fn get_server_info() -> ServerInfo {
+        Self::get_server_info().clone()
     }
 }
